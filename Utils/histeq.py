d698671558f4bdebddecbceb081edaeb29c92fcf
michaelchin <michael.chin@sydney.edu.au>
2015-12-07 14:32:46 +1100

    Add license info

--- Utils/histeq.py
+++ Utils/histeq.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 '''
+	The histeq.py applies histogram equalization to grid data.
+	Copyright (C) 2015 The University of Sydney, Australia
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License, version 2,
+	as published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+	*******************************************************************************
+	
     Filename: histeq.py
     Author: Xiaodong Qin
     Email: michael.chin@sydney.edu.au
